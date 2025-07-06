c043b9cbfc96918b2c2bbe94346129c96694d14c
root <root@DESKTOP-JE1AT0C.localdomain>
2025-07-06 23:50:40 +0300

    Add nginx job

--- pipelines/main01.groovy
+++ pipelines/main01.groovy
@@ -1,7 +1,7 @@
 def githubRepo = 'git@github.com:mottioh/home-assignment.git'
 def credentialsId = 'github-creds'
 
-pipelineJob('Build-Tag-Push') {
+pipelineJob('Build-Tag-Push-Flask') {
     definition {
         cpsScm {
             scm {
@@ -13,9 +13,25 @@ pipelineJob('Build-Tag-Push') {
                     branches('*/main')
                 }
             }
-            scriptPath('pipelines/btp.groovy')
+            scriptPath('pipelines/btp-flask.groovy')
         }
     }
     description('Builds Tag and push reg list Docker to Docker Hub')
 }
-
+pipelineJob('Build-Tag-Push-Nginx') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url(githubRepo)
+                        credentials(credentialsId)
+                    }
+                    branches('*/main')
+                }
+            }
+            scriptPath('pipelines/btp-nginx.groovy')
+        }
+    }
+    description('Builds Tag and pushes a Nginx reverse proxy with source IP header to Docker Hub.')
+}
