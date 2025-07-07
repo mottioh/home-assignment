ca4c4a72ffc00bcfabe0f4730d5784977448dddb
root <root@DESKTOP-JE1AT0C.localdomain>
2025-07-07 23:36:54 +0300

    port update

--- pipelines/runandTest.groovy
+++ pipelines/runandTest.groovy
@@ -41,7 +41,7 @@ pipeline {
             steps {
                 script {
                     sleep(time: 5, unit: "SECONDS") // Give containers time to start
-                    def response = sh (script: "curl -s -o /dev/null -w '%{http_code}' http://localhost:9090/", returnStdout: true).trim()
+                    def response = sh (script: "curl -s -o /dev/null -w '%{http_code}' http://127.0.0.1:9090/", returnStdout: true).trim()
                     echo "HTTP Response Code: ${response}"
                     if (response != "200") {
                         error("Request failed! Nginx or Flask not working as expected.")
